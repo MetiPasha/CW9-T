570dfc7c88ac4264f0a01ef2fe54effb1b5d0c70
itsaeed0 <ashourisaeid78@gmail.com>
2024-10-18 23:57:50 +0330

    5 to 9 in completed

--- q5/q5.js
+++ q5/q5.js
@@ -14,24 +14,65 @@
 
 //q6
 
-const listStudent = [
-    Ali: {
-        math:'18',
-        science: '20',
-        physic:'16',
-        chemistry: '17';
-    },
+// const listStudent = [
+//     {name: "Ali", grade:[19,15,6,13]},
+//     {name: "Amir", grade:[20, 13, 16, 18]},
+//     {name: "Hashem", grade:[16, 15, 18, 19]}
+// ];
 
-    Aref: {
-        math:'19',
-        science: '18',
-        physic:'12',
-        chemistry: '19';
-    },
-    Amir: {
-        math:'15',
-        science: '19',
-        physics: '13',
-        chemistry: '18',
-    }
-];
+// const result = listStudent.map(({name, grade})=> ({
+//     name, 
+//     avg : grade.reduce((sum, grades) => sum + grades, 0) / grade.length
+// }));
+
+// console.log(result);
+
+
+//q7
+
+// const company = {
+//     name: "Tech Corp",
+//     employees: [{
+//         name: "John", 
+//         department: {
+//             name: "Engineering",
+//             location: "Building A"
+//         }
+//     },
+//     {
+//         name: "Jane",
+//         department: {
+//             name: "Marketing",
+//             location: "Building B"
+//         }
+//     }
+// ]
+// };
+
+// const info = company.employees.map(({name, department:{name: department}}) =>({
+//     name,
+//     department
+// }))
+// console.log(info);
+
+//q8
+
+// const  foods = ["Burger, Pizza, Sushi, Sezar"];
+// const drink = ["Soda", "Lemonade", "Sprite", "Sperso"];
+
+// const merging = [...foods, ...drink];
+// console.log(merging);
+
+
+//q9
+// const nestedArray = [1, [2, 3], [4, [5, 6]]];
+
+// const flatern = [];
+
+// for(const element of nestedArray){
+//     flatern.push(...element);
+
+// }
+// console.log(flatern);
+
+//q10
