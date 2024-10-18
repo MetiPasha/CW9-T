c6389b3b6920f8edb310396b87c605afc2c59edb
itsaeed0 <ashourisaeid78@gmail.com>
2024-10-18 19:18:06 +0330

    question 5

---
+++ q5/q5.js
@@ -0,0 +1,37 @@
+//q5
+
+// const numbers = [25, 80, 45];
+// const [small, mid, big] = numbers.sort((a,b)=> a - b);
+
+
+// const result = {
+//     small,
+//     mid,
+//     big
+// };
+// console.log(result);
+
+
+//q6
+
+const listStudent = [
+    Ali: {
+        math:'18',
+        science: '20',
+        physic:'16',
+        chemistry: '17';
+    },
+
+    Aref: {
+        math:'19',
+        science: '18',
+        physic:'12',
+        chemistry: '19';
+    },
+    Amir: {
+        math:'15',
+        science: '19',
+        physics: '13',
+        chemistry: '18',
+    }
+];
